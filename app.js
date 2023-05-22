6162f1272e4024969071d9f6de19d009de86fc5c
Frank <frankivil89@gmail.com>
2023-05-22 16:00:55 -0500

    Ultimos cambios

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 console.log('Hola');
 console.log('Prueba conflicto');
 console.log('Hola frank');
-consolo.log("Hola prueba de la nueva")
+consolo.log('Hola prueba de la nueva');
 
-
-for i in range:
-	print("Hola")
+// for i in range:
+// 	print("Hola")
