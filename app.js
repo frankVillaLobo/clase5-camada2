e2eeec6ce6e3520aea9617ddb049cd3975ced312
Frank <frankivil89@gmail.com>
2023-05-22 12:41:33 -0500

    Resolviendo los conflictos

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
 console.log('Hola');
-<<<<<<< HEAD
 console.log('Prueba conflicto');
-=======
-console.log("Hola frank")
->>>>>>> f0f684c8e227ff0451bffcf2bc2fcc01bb46d886
+console.log('Hola frank');
